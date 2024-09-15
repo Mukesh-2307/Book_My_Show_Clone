0d6729b97e602dc973a6bbc680ebd9e85c11c1ac
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-09-15 11:46:39 +0000

    update till day 04

--- book_my_show_clone/src/App.js
+++ book_my_show_clone/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 
+// pages
+import HomePage from "./pages/Home.page"
+import MoviePage from "./pages/Movie.page"
+import Plays from "./pages/Play.page"
+
 // react-slick
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,11 +14,11 @@ function App() {
   return (
     <>
     <Routes>
-      {/* <Route path='/' element={<HomePage/>}/>
+      <Route path='/' element={<HomePage/>}/>
       <Route path='/movie/:id' element={<MoviePage/>}/>
-      <Route path='/plays' element={<Plays/>}/> */}
+      <Route path='/plays' element={<Plays/>}/>
     </Routes>
-    <h1 className='text-3xl font-bold underline'>Hello world !</h1>
+    {/* <h1 className='text-3xl font-bold underline'>Hello world !</h1> */}
     </>
   );
 }
