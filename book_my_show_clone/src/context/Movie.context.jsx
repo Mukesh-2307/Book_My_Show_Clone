bc6247a8bdb4e82779911f89e04edfcf7a8cf6cd
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-09-20 06:32:29 +0000

    added context for movie

---
+++ book_my_show_clone/src/context/Movie.context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const MovieContext = createContext(); // Ensure export is correct
+
+const MovieProvider = ({ children }) => {
+  const [movie, setMovie] = useState({
+    id: 0,
+    originalTitle: "",
+    overview: "",
+    imageSet: "",
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <MovieContext.Provider value={{ movie, setMovie, loading, setLoading }}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
+
+export default MovieProvider;
